862300bcb2468f317f031d6c0efe9c97340c6255
hello-sahasraka <sahasrakasachin@gmail.com>
2025-10-29 17:23:37 +0530

    fix: typo correction 2

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -28,7 +28,7 @@ export const AboutSection = () => {
                         <a href="#contact" className="cosmic-button"> Get In Touch</a>
                         <a
                             href="Sahasraka_sachin.pdf"
-                            download="Sahasraka_Sachin_Resume.pdf"
+                            download="Sahasraka_Sachin_CV.pdf"
                             className={cn(
                                 "px-6 py-2 rounded-full border border-primary",
                                 "text-primary hover:bg-primary/40 transition-all duration-300"
